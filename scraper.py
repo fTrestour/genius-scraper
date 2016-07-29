1b9ef59fc743bd7cd724fec46f69db719402d102
fTrestour <ftrestour@gmail.com>
2016-07-29 13:16:42 +0200

    It's alive!

--- scraper.py
+++ scraper.py
@@ -13,17 +13,11 @@ artist = "Vald"
 song = "bonjour"
 artist_url = BASE_URL + "artists/" + artist + "/"
 song_url = BASE_URL + artist + "-" + song + "-lyrics"
-
 print "\nOpening " + song_url
-response = requests.get(artist_url, headers={'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'})
-content = unidecode(response.text)
-soup = BeautifulSoup(content, 'html.parser')
 
-lyrics = soup.findAll("lyrics")
-print lyrics
-
-# find_all("div", class_="sister")
-
-# print "\nLyrics"
-# lyrics = soup.find('lyrics')
-# print lyrics
+response = requests.get(song_url)
+content = response.text
+soup = BeautifulSoup(content)
+soup = soup.find("lyrics")
+soup = BeautifulSoup(soup.prettify())
+print soup.get_text()
